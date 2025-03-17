44941d54d23eaa7d5e0235cd1e76dfa1238aa50d
vegalv <vegard.m.alvestad@gmail.com>
2025-03-17 17:03:53 +0100

    Add directory for non-ELPA/MELPA packages

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Directory with elisp files
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
-
+(add-to-list 'load-path (concat user-emacs-directory "packages"))
 
 (require 'veg-base)
 (require 'veg-functions)
