7110631a78aca06bcfcbc68a5f8c76693fce7f32
vegalv <vegard.m.alvestad@gmail.com>
2025-03-18 23:04:56 +0100

    Move some more functions to veg-functions.

--- elisp/veg-functions.el
+++ elisp/veg-functions.el
@@ -1,5 +1,31 @@
-;; Deleting words without copying to kill-ring
-;; (https://emacs.stackexchange.com/questions/22266/backspace-without-adding-to-kill-ring)
+(defun veg/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'.
+
+Source: https://protesilaos.com/codelog/2024-11-28-basic-emacs-configuration/"
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(define-key global-map (kbd "C-g") #'veg/keyboard-quit-dwim)
+
 (defun veg/delete-word (arg)
   "Delete characters forward until encountering the end of a word.
 With argument, do this that many times.
