96299575965cb22a5f36018f1af40e206c7ec3b8
Vegard Myrland Alvestad <vegarmal@ulrik.uio.no>
2025-03-20 14:52:58 +0100

    disable menu-bar

--- elisp/veg-base.el
+++ elisp/veg-base.el
@@ -269,7 +269,7 @@
            (setq-local electric-pair-inhibit-predicate
                    `(lambda (c)
                       (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
-(menu-bar-mode 1)
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (scroll-bar-mode -1)
