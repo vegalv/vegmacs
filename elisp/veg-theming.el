e388db9ff61f3fcbc64cddd6bd1b7268eafb8882
vegalv <vegard.m.alvestad@gmail.com>
2025-03-25 19:59:33 +0100

    theming: change to ef-themes

--- elisp/veg-theming.el
+++ elisp/veg-theming.el
@@ -2,53 +2,62 @@
 
 ;;; Code:
 
-(require-theme 'modus-themes)
+(use-package ef-themes
+  :ensure t
+  :bind
+  ("C-c m" . 'ef-themes-toggle)
+  :hook
+  (ef-themes-post-load . my-ef-themes-custom-faces)
+  :config
+  (setq ef-themes-mixed-fonts t
+        ef-themes-to-toggle '(ef-dream ef-reverie))
 
-(setq modus-themes-mixed-fonts t
-      modus-themes-org-blocks 'default
-      modus-themes-to-toggle '(modus-vivendi modus-operandi-tinted)
-      modus-themes-italic-constructs nil
-      modus-themes-bold-constructs t
-      modus-themes-common-palette-overrides modus-themes-preset-overrides-faint)
-
-(let ((mono-spaced-font "Aporetic Sans Mono")
+  (let ((mono-spaced-font "Aporetic Sans Mono")
 	(proportionally-spaced-font "Aporetic Sans"))
-  (set-face-attribute 'default nil :family mono-spaced-font :height 160)
-  (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 160)
-  (set-face-attribute 'variable-pitch nil :family proportionally-spaced-font :height 1.0)
+    (set-face-attribute 'default nil :family mono-spaced-font :height 160)
+    (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 160)
+    (set-face-attribute 'variable-pitch nil :family proportionally-spaced-font :height 1.0)
 
-    (defun my-modus-themes-custom-faces ()
-    "My customizations on top of the Modus themes.
-This function is added to the `modus-themes-post-load-hook'."
-    (modus-themes-with-colors
-      (custom-set-faces
-       ;;; core
-       `(fringe ((,c :background ,bg-main)))
-       `(line-number ((,c :background ,bg-main)))
-       ;; highlight-indentation-mode
-       ;; `(highlight-indentation-face ((,c :background ,bg-main)))
-       ;; dashboard
-       `(dashboard-text-banner ((,c :weight bold :foreground ,magenta-faint)))
-       `(dashboard-heading ((,c :weight bold :foreground ,magenta-faint)))
-       ;; ivy
-       `(ivy-modified-buffer ((,c :foreground ,magenta)))
-       `(ivy-org ((,c :foreground ,blue-faint :weight normal)))
-       `(ivy-subdir ((,c :foreground ,cyan-faint)))
-       ;; mood-line
-       `(mood-line-buffer-status-modified ((,c :foreground ,fg-alt)))
-       ;; olivetti-mode
-       `(olivetti-fringe ((,c :background ,bg-main)))
-       ;; org
-       `(org-block ((,c :background ,bg-dim :extend t)))
-       `(org-tag ((,c :foreground ,fg-dim :weight bold)))))))
-
-(global-set-key (kbd "C-c m") 'modus-themes-toggle)
-
-;; Using the hook lets our changes persist when we use the commands
-;; `modus-themes-toggle' and `modus-themes-select'.
-(add-hook 'modus-themes-post-load-hook #'my-modus-themes-custom-faces)
-(modus-themes-select 'modus-vivendi)
-
-(load-theme 'modus-vivendi)
+    (defun my-ef-themes-custom-faces ()
+      "My customizations on top of the Ef themes.
+This function is added to the `ef-themes-post-load-hook'."
+      (ef-themes-with-colors
+       (custom-set-faces
+        ;;; core
+        `(fringe ((,c :background ,bg-main)))
+        `(line-number ((,c :background ,bg-main)))
+        ;;; dashboard
+        `(dashboard-text-banner ((,c :weight bold :foreground ,magenta-faint)))
+        `(dashboard-heading ((,c :weight bold :foreground ,magenta-faint)))
+        ;;; dired
+        `(dired-directory ((,c :weight bold)))
+        ;;; ivy
+        `(ivy-current-match ((,c :inherit swiper-line-face)))
+        `(ivy-minibuffer-match-face-1 ((,c :inherit swiper-match-face-1 )))
+        `(ivy-minibuffer-match-face-2 ((,c :inherit swiper-match-face-2)))
+        `(ivy-minibuffer-match-face-3 ((,c :inherit swiper-match-face-3)))
+        `(ivy-minibuffer-match-face-4 ((,c :inherit swiper-match-face-4)))
+        `(ivy-org ((,c :inherit org-level-1 :weight normal)))
+        ;; `(ivy-subdir ((,c  :weight bold)))
+        ;;; mood-line
+        `(mood-line-buffer-status-modified ((,c :foreground ,fg-alt)))
+        ;; olivetti-mode
+        `(olivetti-fringe ((,c :background ,bg-main)))
+        ;;; org
+        `(org-block ((,c :background ,bg-dim)))
+        `(org-block-begin-line ((,c :background ,bg-main)))
+        `(org-block-end-line ((,c :background ,bg-main)))
+        ;;; swiper
+        `(swiper-match-face-1 ((,c :background nil :foreground ,nil)))
+        `(swiper-match-face-2 ((,c :weight bold :background nil :foreground ,magenta-warmer)))
+        `(swiper-match-face-3 ((,c :weight bold :background nil :foreground ,yellow-cooler)))
+        `(swiper-match-face-4 ((,c :weight bold :background nil :foreground ,blue-warmer)))
+        `(swiper-line-face ((,c :background ,bg-hl-line :extend t)))
+        `(swiper-background-match-face-1 ((,c :inherit swiper-match-face-1)))
+        `(swiper-background-match-face-2 ((,c :inherit swiper-match-face-2)))
+        `(swiper-background-match-face-3 ((,c :inherit swiper-match-face-3)))
+        `(swiper-background-match-face-4 ((,c :inherit swiper-match-face-4))))))))
+(ef-themes-select 'ef-dream)
 
 (provide 'veg-theming)
+;;; veg-theming.el ends here
